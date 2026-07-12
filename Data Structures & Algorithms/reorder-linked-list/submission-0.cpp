e6163565b1ef6d2675b941a58c1708b0d229e5f5
Sandi <81544477+setiawansandi@users.noreply.github.com>
2026-07-12 22:37:26 +0800

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        // get midpoint
+        ListNode *slow = head, *fast = head;
+
+        while(fast && fast->next && fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // reverse 2nd half
+        ListNode *curr = slow->next;
+        slow->next = nullptr;
+
+        ListNode *prev = nullptr;
+        while(curr) {
+            ListNode *temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        ListNode* list1 = head;
+        ListNode* list2 = prev;
+        // combine
+        while(list2) {
+            ListNode *temp1 = list1->next;
+            ListNode *temp2 = list2->next;
+            list1->next = list2;
+            list2->next = temp1;
+            list2 = temp2;
+            list1 = temp1;
+        }        
+
+    }
+};
+
+
+/*
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr, *current = head;
+
+        while(current) {
+            ListNode* temp = current->next;
+            current->next = prev;
+            prev = current;
+            current = temp;
+        }
+        return prev;
+    }
+*/
